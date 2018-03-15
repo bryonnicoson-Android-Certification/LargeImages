99d5a19fe13cc630d29e86955b511ededcbf6976
Bryon Nicoson <bryon.nicoson@gmail.com>
2018-03-15 10:48:54 -0500

    Initial commit

---
+++ app/src/main/java/com/bryonnicoson/largeimages/MainActivity.java
@@ -0,0 +1,31 @@
+package com.bryonnicoson.largeimages;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int toggle = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void swapImage(View view) {
+        if (toggle == 0) {
+            view.setBackgroundResource(R.drawable.dinosaur_medium);
+            toggle = 1;
+        } else {
+            try {
+                Thread.sleep(32);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            view.setBackgroundResource(R.drawable.ankylo);
+            toggle = 0;
+        }
+    }
+}
